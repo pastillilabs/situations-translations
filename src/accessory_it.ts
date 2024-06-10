209f40296c7ad3a3c523b2b37317ee114d0db74a
Heikki Haveri <heikki.haveri@pastillilabs.com>
2024-06-10 04:51:12 +0300

    Initial Qt6 & cmake

---
+++ src/accessory_it.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name></name>
+    <message id="accessory_name">
+        <source>Accessory</source>
+        <translation>Accessori</translation>
+    </message>
+    <message id="accessory_description">
+        <source>Connected accessories</source>
+        <translation type="unfinished">Connected accessories</translation>
+    </message>
+    <message id="accessory_audio">
+        <source>Audio</source>
+        <translation>Audio</translation>
+    </message>
+    <message id="accessory_video">
+        <source>Video</source>
+        <translation>Video</translation>
+    </message>
+    <message id="accessory_carkit">
+        <source>Car kit</source>
+        <translation>Kit auto</translation>
+    </message>
+    <message id="accessory_charger">
+        <source>Charger</source>
+        <translation>Caricabatterie</translation>
+    </message>
+</context>
+</TS>
